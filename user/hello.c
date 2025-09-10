361f77ae1006de15b2f48b9767f0a00242bb9725
Seunghun Lee <lees254@unlv.nevada.edu>
2025-09-09 19:58:47 -0700

    Add hello user program (proj1)

---
+++ user/hello.c
@@ -0,0 +1,9 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+int main (int argc, char *argv[]) {
+printf ("Hello world, this is Seunghun Lee.\n");
+printf ("I am in CS 370, Fall 2025, section 1001.\n\n");
+exit(0);
+}
